890bf077907de9a84ac2e92749549613517f3606
Eddie <mahamaek@gmail.com>
2020-12-30 12:52:47 -0500

    added memo

--- factorial.py
+++ factorial.py
@@ -1,4 +1,7 @@
-def fact(n):
+def fact(n, m={}):
     if n == 0 or n == 1:
-        return 1
-    return n*fact(n-1)
+        m[n] = 1
+        return m[n]
+    if n not in m:
+        m[n] = n*fact(n-1)
+    return m[n]
